5966a7534685848c5ad1aa5e9aaed7c267ed9a7a
sagdish <forsi1@yahoo.com>
2020-09-16 16:49:09 -0700

    bft search

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,8 +3,8 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
-class Graph:
 
+class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
@@ -13,8 +13,8 @@ class Graph:
         self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
-        # if v1 or v2 not in self.vertices:
-        #     return "vertex does exist"
+        if (v1 or v2) not in self.vertices:
+            return "vertex does exist"
         self.vertices[v1].add(v2)
 
     def get_neighbors(self, vertex_id):
@@ -25,7 +25,21 @@ class Graph:
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        q = Queue()
+        q.enqueue(starting_vertex)
+        visited = set()
+
+        while q.size() > 0:
+            current_vertex = q.dequeue()
+            if current_vertex not in visited:
+                print(f'vertex: {current_vertex}')
+                visited.add(current_vertex)
+
+                edges = self.get_neighbors(current_vertex)
+                for vertex in edges:
+                    q.enqueue(vertex)
+                # for vertex in self.get_neighbors(current_vertex):
+                #     q.enqueue(vertex)
 
     def dft(self, starting_vertex):
         """
@@ -69,6 +83,7 @@ class Graph:
         """
         pass  # TODO
 
+
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
     # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
@@ -89,13 +104,11 @@ if __name__ == '__main__':
     graph.add_edge(3, 5)
     graph.add_edge(2, 3)
     graph.add_edge(4, 6)
-
     '''
     Should print:
         {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
     '''
     print(graph.vertices)
-
     '''
     Valid BFT paths:
         1, 2, 3, 4, 5, 6, 7
@@ -112,7 +125,6 @@ if __name__ == '__main__':
         1, 2, 4, 3, 7, 5, 6
     '''
     graph.bft(1)
-
     '''
     Valid DFT paths:
         1, 2, 3, 5, 4, 6, 7
@@ -122,13 +134,11 @@ if __name__ == '__main__':
     '''
     graph.dft(1)
     graph.dft_recursive(1)
-
     '''
     Valid BFS path:
         [1, 2, 4, 6]
     '''
     print(graph.bfs(1, 6))
-
     '''
     Valid DFS paths:
         [1, 2, 4, 6]
