45313821ed869a789d04d8c808072b3b4a1ae3bb
sagdish <forsi1@yahoo.com>
2020-09-24 13:16:13 -0700

    ancestors solution

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -18,19 +18,29 @@ class Queue():
 def earliest_ancestor(ancestors, starting_node):
     q = Queue()
     q.enqueue(starting_node)
+    parents = []
 
     while q.size() > 0:
         currentNode = q.dequeue()
-        parent = -1
-        parent2 = -1
         gen = 0
 
         for pair in ancestors:
             if (currentNode == pair[1]):
-                parent = pair[0]
+                gen += 1
+                print('reached')
+                if gen > 1:
+                    if parents[-1] < pair[0]:
+                        temp = parents[-1]
+                        parents[-1] = pair[0]
+                        parents.append(temp)
+                        continue
+                parents.append(pair[0])
+                print(parents)
+                q.enqueue(pair[0])
 
-    return parent
+    return parents[-1] if len(parents) > 0 else -1
 
 
-earliest_ancestor([(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8),
-                   (8, 9), (11, 8), (10, 1)], 6)
+print(
+    earliest_ancestor([(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8),
+                       (8, 9), (11, 8), (10, 1)], 1))
